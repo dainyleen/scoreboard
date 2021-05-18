9823d3d2ef62395ee57bc25d933b46cacfd4f633
Dainyleen Masangkay <dainyleen@gmail.com>
2021-05-19 00:24:06 +1200

    Maker number of players used props and shows dynamic data now

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,17 +3,19 @@ import React from 'react'
 import Header from './Header'
 import Player from './Player'
 
-const App = () => {
+const App = (props) => {
   return (
     <div className="scoreboard">
       <Header
         title="Scoreboard"
-        totalPlayers={1}
-      />,
-      <Player
-        name="Dainy"
-        score={5}
+        totalPlayers={props.initialPlayers.length}
       />
+      {props.initialPlayers.map(player =>
+        <Player
+          name={player.name}
+          score={player.score}
+        />
+      )}
     </div>
   )
 }
--- client/index.js
+++ client/index.js
@@ -3,9 +3,28 @@ import ReactDOM from 'react-dom'
 
 import App from './components/App'
 
+const players = [
+  {
+    name: 'Dainy',
+    score: 5
+  },
+  {
+    name: 'Faye',
+    score: 10
+  },
+  {
+    name: 'Kapi',
+    score: 15
+  },
+  {
+    name: 'Mirris',
+    score: 20
+  }
+]
+
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <App />,
+    <App initialPlayers={players} />,
     document.getElementById('app')
   )
 })
