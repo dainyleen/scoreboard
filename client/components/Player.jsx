9927a48516aa8f76ce1212f14b25a34d3c990a27
Dainyleen Masangkay <dainyleen@gmail.com>
2021-05-18 19:22:37 +1200

    Work in progress

--- client/components/Player.jsx
+++ client/components/Player.jsx
@@ -10,6 +10,7 @@ const Player = () => {
       </div>
       <div className="counter">
         <button className="counter-action decrement"> - </button>
+        <span className="counter-score">25</span>
         <button className="counter-action increment"> + </button>
       </div>
     </div>
