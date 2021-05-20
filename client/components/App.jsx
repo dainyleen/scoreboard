21d36263d543e785f432224166f0ed19923a8813
Dainyleen Masangkay <dainyleen@gmail.com>
2021-05-20 22:12:27 +1200

    Converted App.jsx from functional to a class component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,21 +3,44 @@ import React from 'react'
 import Header from './Header'
 import Player from './Player'
 
-const App = (props) => {
-  return (
-    <div className="scoreboard">
-      <Header
-        title="Scoreboard"
-        totalPlayers={props.initialPlayers.length}
-      />
-      {props.initialPlayers.map(player =>
-        <Player
-          key={player.id}
-          name={player.name}
+class App extends React.Component {
+  state = {
+    players: [
+      {
+        id: 1,
+        name: 'Dainy'
+      },
+      {
+        id: 2,
+        name: 'Faye'
+      },
+      {
+        id: 3,
+        name: 'Kapi'
+      },
+      {
+        id: 4,
+        name: 'Mirris'
+      }
+    ]
+  }
+
+  render () {
+    return (
+      <div className="scoreboard">
+        <Header
+          title="Scoreboard"
+          totalPlayers={props.initialPlayers.length}
         />
-      )}
-    </div>
-  )
+        {props.initialPlayers.map(player =>
+          <Player
+            key={player.id}
+            name={player.name}
+          />
+        )}
+      </div>
+    )
+  }
 }
 
 export default App
